81335d80603e2506ff7b772ba0771d46299762c2
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-05-30 15:12:17 +0900

    feat: 기본 세팅
    mysql 연동
    
    klusterWeb Spring boot 설치

---
+++ KlusterWeb/src/main/java/kluster/klusterweb/KlusterWebApplication.java
@@ -0,0 +1,13 @@
+package kluster.klusterweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlusterWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlusterWebApplication.class, args);
+	}
+
+}
