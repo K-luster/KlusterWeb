ad2f23be513498080f003b6d35daaca20d4cfe74
gurwls0122 <80228712+gurwls0122@users.noreply.github.com>
2023-09-19 15:36:01 +0900

    Feat : kluster api 연결(pod 정보 받아오기)

---
+++ KlusterWeb/src/main/java/kluster/klusterweb/config/RestTemplateConfig.java
@@ -0,0 +1,54 @@
+package kluster.klusterweb.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+@Configuration("restTemplateConfigCommon")
+public class RestTemplateConfig {
+
+    @Bean
+    @Qualifier("restTemplateCommon")
+    public RestTemplate restTemplate(RestTemplateBuilder builder) throws NoSuchAlgorithmException, KeyManagementException
+    {
+
+        TrustManager[] trustAllCerts = new TrustManager[] {
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                    public void checkClientTrusted(
+                            X509Certificate[] certs, String authType) {
+                    }
+                    public void checkServerTrusted(
+                            X509Certificate[] certs, String authType) {
+                    }
+                }
+        };
+//        SSLContext sslContext = SSLContext.getInstance("SSL");
+        //tls 버전 확인해야 kuber api 불러올 수 있음
+        SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLContext(sslContext)
+                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                .build();
+        HttpComponentsClientHttpRequestFactory customRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        customRequestFactory.setHttpClient(httpClient);
+        return builder.requestFactory(() -> customRequestFactory).build();
+    }
+}
